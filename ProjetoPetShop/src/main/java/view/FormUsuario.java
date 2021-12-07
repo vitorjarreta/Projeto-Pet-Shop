89bfc16c6733babc7690b5c1f6e36237c812c3a7
GuerreraProgram <felipe.guerrera@hotmail.com>
2021-12-07 15:51:23 -0300

    esqueci de salvar

--- ProjetoPetShop/src/main/java/view/FormUsuario.java
+++ ProjetoPetShop/src/main/java/view/FormUsuario.java
@@ -82,6 +82,11 @@ public class FormUsuario extends javax.swing.JFrame {
         jButton3.setForeground(new java.awt.Color(255, 255, 0));
         jButton3.setText("Voltar");
         jButton3.setBorder(null);
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -157,6 +162,10 @@ public class FormUsuario extends javax.swing.JFrame {
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         this.dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
